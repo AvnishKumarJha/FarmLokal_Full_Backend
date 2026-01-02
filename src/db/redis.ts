5cfbc020e85cb49a80b4ddf3600105500eb1ea50
Avnish Kumar Jha <avnishbtech1@gmail.com>
2026-01-02 23:52:29 +0530

    Fix Redis config and token caching

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,4 +1,4 @@
 import Redis from "ioredis";
 import { config } from "../config";
 
-export const redis = new Redis(config.redisUrl);
+export const redis = new Redis(process.env.REDIS_URL as string);
